2094d2c83b0506439cd10d3ea9290589bc2ebba9
Macdonald <dale.macdonald@thomsonreuters.com>
2019-03-12 09:21:14 +0000

    Upgrade to version 12

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V11 - We see Marvin the Paranoid Monkey!' });
+  res.render('index', { title: 'Express V12 - Behold Marvin the Paranoid Android!' });
 });
 
 module.exports = router;
