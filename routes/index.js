a7156aa0775e45f015228cda7dcb1b223ecaa948
Macdonald <dale.macdonald@thomsonreuters.com>
2019-03-12 00:38:08 +0000

    Version 11 Deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V10b - All hail Marvin the Cheeky Monkey!' });
+  res.render('index', { title: 'Express V11 - We see Marvin the Paranoid Monkey!' });
 });
 
 module.exports = router;
