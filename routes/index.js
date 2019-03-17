822670b2331ee97f2c23aff0993279b3a6c9abad
dalehome <dale_macdonald@hotmail.com>
2019-03-17 21:05:04 +0000

    Update to version 14

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V13 - It is Marvin the Paranoid Android! - This is grrrreat!' });
+  res.render('index', { title: 'Express V14 - It is Garvin the Paranoid Peanut! - This is grrrreat!' });
 });
 
 module.exports = router;
