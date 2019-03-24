16bfb1c67fa5cf577a00035b0749c491e36b6b1a
Daleman <dalemanus@gmail.com>
2019-03-24 19:01:33 +0000

    Upgrade to version 15

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V14 - It is Garvin the Paranoid Peanut! - This is grrrreat!' });
+  res.render('index', { title: 'Express V15 - It is Starvin Marvin the Paranoid Pirate! - This is grrrreat!' });
 });
 
 module.exports = router;
